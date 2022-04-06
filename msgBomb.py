07d86e249b7af045aa200c057934f6b18a36efdb
d4kox <101132138+d4kox@users.noreply.github.com>
2022-04-05 22:36:26 -0300

    Add files via upload

---
+++ msgBomb.py
@@ -0,0 +1,10 @@
+import pyautogui as pg, time as tm, os 
+msg_counter = input("Cantidad de mensajes >> ")
+msg_counter = int(msg_counter)
+msg_text = input("Texto del mensaje >> ")
+print('\n [!] Clickea el campo en WhatsApp Web que dice "Escribe un mensaje aquí"')
+print(" [*] Los mensajes se enviaran en 5 segundos...")
+tm.sleep(5)
+for a in range(0,msg_counter):
+    pg.write(msg_text)
+    pg.press('enter')
